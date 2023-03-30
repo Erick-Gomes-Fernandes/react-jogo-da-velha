2152ab4b0d99594d41fa0d901718215dd0f65124
Erick Gomes Fernandes <erick.gomes.fernandes.egf@gmail.com>
2023-03-30 17:27:14 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from './Header.module.css'
 
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
+import Icon from '../icon/icon'
 
 function Header () {
   return(
     <div className={styles.header}>
       <Title>Jogo da Véia</Title>
       <Subtitle>Criado por Erick Gomes</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/Erick-Gomes-Fernandes"/>
+      </div>
     </div>
   )
 }
